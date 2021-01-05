1ccc6b3ecd654979d23d9d276a2202fd271d2710
Chigozie Orunta <chigozieorunta@yahoo.com>
2021-01-05 10:59:57 +0100

    Fixed linting and syntax issues

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,22 @@
-// Get Gulp module
+// Get Gulp module.
 const { src, dest, watch } = require('gulp');
 
-//Get minify, autoprefixer and sass modules
+// Get minify, autoprefixer and sass modules.
 const minify = require('gulp-clean-css');
 const autoprefixer = require('gulp-autoprefixer');
 const sass = require('gulp-sass')(require('sass'));
 
-//Set up paths
+// Set up paths.
 const paths = {
 	src: {
 		css: './assets/css/src/**/*.scss',
-		js: './assets/js/src/**/*.js'
+		js: './assets/js/src/**/*.js',
 	},
 	dest: {
 		css: './assets/css/dist',
-		js: './assets/js/dist'
-	}
-}
+		js: './assets/js/dist',
+	},
+};
 
 // Compile script
 const compile = () => {
@@ -25,12 +25,12 @@ const compile = () => {
 		.pipe(autoprefixer())
 		.pipe(minify())
 		.pipe(dest(paths.dest.css));
-}
+};
 
 // Watch script
 const observe = () => {
 	watch(paths.src.css, compile);
-}
+};
 
 exports.sass = compile;
 exports.watch = observe;
